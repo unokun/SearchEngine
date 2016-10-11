74f3ca0aa6dc46bd98c6b5e95c02fac770dbbf92
Masaaki Uno <unokun@gmail.com>
2016-10-12 07:17:17 +0900

    add test

--- test/indexer/ScannerTest.java
+++ test/indexer/ScannerTest.java
@@ -1,5 +1,20 @@
 package indexer;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class ScannerTest {
+	Scanner scanner;
+	
+	@Before
+	public void setUp() {
+		scanner = new LFSScanner();
+	}
+	@Test
+	public void testScan() {
+		scanner.scan(".");
+	}
 
 }
